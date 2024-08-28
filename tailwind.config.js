d47b756874941afd66fb973540599f2d3a1ca216
vivekjami <j.vivekvamsi@gmail.com>
2024-08-28 16:34:41 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}', // add these file extensions if you're using TypeScript
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
